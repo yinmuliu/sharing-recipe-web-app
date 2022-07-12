a0caaa2765fb484de7019b570528203697adbdc3
Yinmu Liu <yinmu.liu@gmail.com>
2022-07-12 18:18:56 +1000

    complete front-end js for adding ingredients/methods

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,55 @@
-// ============== INGREDIENTS & METHODS INPUT ================ //
-// Select elements
-const addIngredientsButton = document.querySelector('.multi-ingredients')
-const addMethodsButton = document.querySelector('.multi-methods')
-
-// When onclick, another textarea input is added.
-// const addIngredientsInput = () => {
-    
-// }
-
-// addIngredientsButton.addEventListener('click', addIngredientsInput)
-
 // ============== UPLOAD AND DISPLAY IMAGE ================ //
 const recipeImgTarget = document.querySelector('.recipe_img_target')
 const imgInput = document.querySelector('#img')
 
 const updateRecipeImg = () => {
     let newRecipeImg = window.URL.createObjectURL(imgInput.files[0]);
-    console.log(newRecipeImg);
     recipeImgTarget.src = newRecipeImg;
 }
 
 if (imgInput !== null) {
     imgInput.addEventListener('change', updateRecipeImg)
 }
+
+// ============== INGREDIENTS & METHODS INPUT ================ //
+const addIngredientsSign = document.querySelector('.multi-ingredients')
+const addMethodsSign = document.querySelector('.multi-methods')
+const ingredientsDiv = document.querySelector('.ingredients')
+const methodsDiv = document.querySelector('.methods')
+
+// When onclick, another textarea input is added.
+const addIngredientsInput = () => {
+    // create a new input textarea
+    const newInput = document.createElement('input')
+    // add class/attributes to the input
+    Object.assign(newInput, {
+        type: 'textarea',
+        name: 'ingredients',
+        id: 'ingredients',
+        // value: "<%= recipe.ingredients %>" for EDIT Page
+        // CONNECTING TO BACKEND - Loop?
+    })
+    // append it to the div
+    ingredientsDiv.insertBefore(newInput, addIngredientsSign)
+}
+
+const addMethodsInput = () => {
+    const newInput = document.createElement('input')
+    Object.assign(newInput, {
+        type: 'textarea',
+        name: 'methods',
+        id: 'methods',
+        // value: "<%= recipe.methods %>"
+        // CONNECTING TO BACKEND - Loop?
+    })
+    methodsDiv.insertBefore(newInput, addMethodsSign)
+}
+
+if (addIngredientsSign !== null && addMethodsSign !== null) {
+    addIngredientsSign.addEventListener('click', addIngredientsInput)
+    addMethodsSign.addEventListener('click', addMethodsInput)
+}
+
+
 
 
